bfddc1f9a778d9e5abcb2a4f17430e6b3146ea40
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2026-07-21 16:05:00 -0400

    Merged

--- aviary/mission/energy_state_problem_configurator.py
+++ aviary/mission/energy_state_problem_configurator.py
@@ -340,11 +340,7 @@ class EnergyStateProblemConfigurator(ProblemConfiguratorBase):
 
             # TODO: replace hard_coded ref for this constraint.
             eq.add_eq_output(
-<<<<<<< HEAD
                 'mass', eq_units='lbm', normalize=False, ref=1.0, add_constraint=True
-=======
-                'mass', eq_units='lbm', normalize=False, ref=1, add_constraint=True
->>>>>>> f5c6d08bd0ab061a4cf3189c8985385ce75dd796
             )
 
             aviary_group.connect(
